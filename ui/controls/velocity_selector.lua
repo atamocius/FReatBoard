53ed90b04277a1c4eef5f8c3727508a4a3b6fb88
Daniel Anton Mata <anton.mata@gmail.com>
2021-08-25 05:45:23 +0800

    Added velocity selector

--- ui/controls/velocity_selector.lua
+++ ui/controls/velocity_selector.lua
@@ -1,38 +1,26 @@
+local velocities = require('config/velocities')
+
 local velocity_selector = {}
 
 local BTN_SPACING_X = 4
 local BTN_W = 28
 
 local function drawButtons(x, y, z)
-    local id = 0
-
-    for i = 0, 11 do
-        id = id + 10
-        local name = 'btnVel' .. id
+    for i = 1, #velocities do
+        local v = velocities[i]
+        local name = 'btnVel' .. v
         GUI.New(name, 'Button', {
             z = z,
-            x = x + i * (BTN_W + BTN_SPACING_X),
+            x = x + (i - 1) * (BTN_W + BTN_SPACING_X),
             y = y,
             w = BTN_W,
             h = 24,
-            caption = '' .. id,
+            caption = '' .. v,
             font = 3,
             col_txt = 'txt',
             col_fill = 'elm_frame'
         })
     end
-
-    GUI.New('btnVel127', 'Button', {
-        z = z,
-        x = x + 12 * (BTN_W + BTN_SPACING_X),
-        y = y,
-        w = 28,
-        h = 24,
-        caption = '127',
-        font = 3,
-        col_txt = 'txt',
-        col_fill = 'elm_frame'
-    })
 end
 
 function velocity_selector.render(x, y, z)
