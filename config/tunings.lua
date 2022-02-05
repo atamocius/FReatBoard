b20bb4c720b9f1f9e5689d41dbeee83c08045b4d
Daniel Anton Mata <anton.mata@gmail.com>
2022-02-05 18:51:10 +0800

    Added more tunings

--- config/tunings.lua
+++ config/tunings.lua
@@ -9,7 +9,6 @@ local SEPARATOR = {
 }
 
 local list = {
-    SEPARATOR,
     -- Guitar - 7 strings
     {
         name = '[GTR 7, 24] Drop A',
@@ -130,7 +129,43 @@ local list = {
             21, -- A0
         },
         frets = 24,
-    }
+    },
+    {
+        name = '[BASS 6, 24] Drop A#/Bb',
+        pitches = {
+            49, -- C#|Db3
+            44, -- G#|Ab2
+            39, -- D#|Eb2
+            34, -- A#|Bb1
+            29, -- F1
+            22, -- A#|Bb0
+        },
+        frets = 24,
+    },
+    {
+        name = '[BASS 6, 24] Drop B',
+        pitches = {
+            50, -- D3
+            45, -- A2
+            40, -- E2
+            35, -- B1
+            30, -- F#|Gb1
+            23, -- B0
+        },
+        frets = 24,
+    },
+    {
+        name = '[BASS 6, 24] Drop C',
+        pitches = {
+            51, -- D#|Eb3
+            46, -- A#|Gb2
+            41, -- F2
+            36, -- C2
+            31, -- G1
+            24, -- C1
+        },
+        frets = 24,
+    },
 }
 
 local function toDictionary(list)
@@ -152,7 +187,6 @@ local function transformList(list)
 end
 
 local tunings = {
-    SEPARATOR = SEPARATOR,
     asList = list,
     asDictionary = transformList(list),
 }
