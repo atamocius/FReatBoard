5f5f71c61fcc5021c0116004570ba2fa50f07489
Daniel Anton Mata <anton.mata@gmail.com>
2022-02-06 23:20:01 +0800

    Added Harmonic Minor scales

--- config/scales.lua
+++ config/scales.lua
@@ -52,6 +52,36 @@ local list = {
         name = '[PENTA] Minor Pentatonic (Aeolian)',
         degrees ='1, b3, 4, 5, b7',
     },
+
+    -- Harmonic Minor Scales
+    {
+        name = '[HARMM] Harmonic Minor (Aeolian #7)',
+        degrees ='1, 2, b3, 4, 5, b6, 7',
+    },
+    {
+        name = '[HARMM] Locrian #6',
+        degrees ='1, b2, b3, 4, b5, 6, b7',
+    },
+    {
+        name = '[HARMM] Ionian #5 (Augmented Major)',
+        degrees ='1, 2, 3, 4, #5, 6, 7',
+    },
+    {
+        name = '[HARMM] Dorian #4 (Ukrainian Dorian)',
+        degrees ='1, 2, b3, #4, 5, 6, b7',
+    },
+    {
+        name = '[HARMM] Phrygian Dominant (Phrygian #3)',
+        degrees ='1, b2, 3, 4, 5, b6, b7',
+    },
+    {
+        name = '[HARMM] Lydian #2',
+        degrees ='1, #2, 3, #4, 5, 6, 7',
+    },
+    {
+        name = '[HARMM] Super Locrian bb7',
+        degrees ='1, b2, b3, b4, b5, b6, bb7',
+    },
 }
 
 return scaleUtils.transformScalesList(list)
