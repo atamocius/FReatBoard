9556aa04e7d4ca155380df0906eaddda3ba44d6f
Daniel Anton Mata <anton.mata@gmail.com>
2022-02-06 20:01:22 +0800

    Added pentatonic scales and modes

--- config/scales.lua
+++ config/scales.lua
@@ -44,6 +44,37 @@ local list = {
             '1, b2, b3, 4, b5, b6, b7'
         ),
     },
+
+    {
+        name = '[PENTA] Major Pentatonic (Ionian)',
+        rootRelativeIndices = scaleUtils.createAllRootRelativeNoteIndices(
+            '1, 2, 3, 5, 6'
+        ),
+    },
+    {
+        name = '[PENTA] Egyptian (Dorian)',
+        rootRelativeIndices = scaleUtils.createAllRootRelativeNoteIndices(
+            '1, 2, 4, 5, b7'
+        ),
+    },
+    {
+        name = '[PENTA] Blues Minor / Man Gong (Phrygian)',
+        rootRelativeIndices = scaleUtils.createAllRootRelativeNoteIndices(
+            '1, b3, 4, b6, b7'
+        ),
+    },
+    {
+        name = '[PENTA] Blues Major / Ritsusen (Mixolydian)',
+        rootRelativeIndices = scaleUtils.createAllRootRelativeNoteIndices(
+            '1, 2, 4, 5, 6'
+        ),
+    },
+    {
+        name = '[PENTA] Minor Pentatonic (Aeolian)',
+        rootRelativeIndices = scaleUtils.createAllRootRelativeNoteIndices(
+            '1, b3, 4, 5, b7'
+        ),
+    },
 }
 
 return scaleUtils.transformScalesList(list)
